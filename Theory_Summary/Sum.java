f4b87574e6bc7956bad01a868af5d34dcccd6d8c
sangyoungahn <tkddud1237@gmail.com>
2019-08-15 13:09:34 +0900

    모든 수의 합구하기

---
+++ Theory_Summary/Sum.java
@@ -0,0 +1,13 @@
+
+public class Sum {
+
+	public static void main(String[] args) {
+		//1부터 10까지의 합을 구하여라
+		int sum = 0;
+		for(int i=1;i<=10;i++) {
+			sum += i;
+		}
+		System.out.println(sum);
+	}
+
+}
