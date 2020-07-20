49cbcf6e40380908f7451e4ccacd8d8a3493992e
sangyoungahn <tkddud1237@gmail.com>
2020-07-20 23:39:14 +0900

    HashMap 사용

---
+++ Programmers/level1/완주하지못한선수.java
@@ -0,0 +1,14 @@
+package level1;
+
+public class 완주하지못한선수 {
+
+	public static void main(String[] args) {
+		String[] participant = {"mislav", "stanko", "mislav", "ana"};
+		String[] completion = {"stanko", "ana", "mislav"};
+		
+		int participantSize = participant.length;
+		
+		
+	}
+
+}
