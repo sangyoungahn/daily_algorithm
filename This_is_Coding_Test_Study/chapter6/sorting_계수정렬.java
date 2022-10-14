4c09544b15264d41285d827d36c13fde9975f168
sangyoungahn <tkddud1237@gmail.com>
2022-10-14 18:30:09 +0900

    Create sorting_퀵정렬,계수정렬.java

---
+++ This_is_Coding_Test_Study/chapter6/sorting_계수정렬.java
@@ -0,0 +1,22 @@
+package python_for_coding_test.sorting;
+
+public class sorting_계수정렬 {
+    static final int MAX_VALUE = 9;
+
+    public static void main(String[] args) {
+        int n = 15;
+        // 모든 원소의 값이 0보다 크거나 같다고 가정
+        int[] arr = {7, 5, 9, 0, 3, 1, 6, 2, 9, 1, 4, 8, 0, 5, 2};
+        // 모든 범위를 포함하는 배열 선언(모든 값은 0으로 초기화)
+        int[] cnt = new int[MAX_VALUE + 1];
+
+        for(int i=0; i<n; i++) {
+            cnt[arr[i]] += 1;   // 각 데이터에 해당하는 인덱스의 값 증가
+        }
+        for(int i=0; i<=MAX_VALUE; i++) {
+            for(int j=0; j<cnt[i]; j++) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
